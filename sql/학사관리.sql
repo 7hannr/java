948116fe439e53b594b78911b194671d2dbe9811
7hannr <ellen07614@gmail.com>
2024-04-19 18:21:08 +0900

    sql

--- sql/학사관리.sql
+++ sql/학사관리.sql
@@ -9,7 +9,7 @@ create table professors(
 );
 desc professors;
 
-create table students(
+create table oldstudents(
  scode char(8) not null, 
  sname varchar(15) not null, 
  dept varchar(30), 
@@ -36,7 +36,7 @@ create table courses(
 
 desc courses;
 
-create table enrollments(
+create table oldenrollments(
  lcode char(4) not null, 
  scode char(8) not null, 
  edate date,
@@ -218,24 +218,136 @@ where c.lcode=e.lcode and e.scode=s.scode and instructor=pcode and lname='데이터
 select s.scode,s.sname,e.lcode,lname,pname
 from  students s, enrollments e, courses c, professors p
 where  e.scode=s.scode and e.lcode=c.lcode and pcode=instructor and grade >= 80;
+/*3.조회*/
+/*3.교수들의 총 급여액과 평균 급여액을 구하시오.*/
+select sum(salary) salary_sum, avg(salary) salary_avg
+from professors;
+/*전산과 교수들의 총 급여액과 평균 급여액을 구하시오.*/
+select sum(salary) salary_sum, avg(salary) salary_avg
+from professors
+where dept='전산';
+/*수강신청 한 과목들 중에서 최고 점수와 최저 점수를 구하시오.*/
+select max(grade),min(grade)
+from enrollments;
+/*전산과 학생들은 모두 몇 명인지 구하시오.*/
+select count(scode)
+from students
+where dept='전산';
+/*수강신청 한 학생들은 모두 몇 명인지 구하시오.*/
+select count(distinct(scode))
+from enrollments;
+/*각 학과별 학생들의 수를 구하시오.*/
+select dept,count(scode)
+from students
+group by dept
+order by count(scode) desc;
+/*교수들을 소속학과별, 직급별로 분류하여 각 직급별 평균 급여액수를 구하시오.*/
+select dept,title,avg(salary),count(pcode)
+from professors
+group by dept,title
+order by dept,title; 
+/*각 학생들에 대해 학번, 학생이름, 수강신청 과목들의 평균 점수를 구하시오.*/
+select e.scode, sname,s.dept, avg(grade), count(lcode), p.pname
+from students s,enrollments e, professors p
+where s.scode=e.scode and p.pcode=advisor
+group by e.scode, sname,s.dept, pname /* group by랑 select랑 같아야됨 계산함수는 없어도 됨 */
+order by avg(grade) desc;
+
+create view enr_stu_cou as
+select e.*, s.sname, c.lname
+from enrollments e, students s, courses c
+where e.scode=s.scode and e.lcode=c.lcode;
+
+select * from enr_stu_cou;
 
 
+create view enr_stu_pro as
+select e.*, p.pname, s.sname, s.dept
+from enrollments e, students s,professors p
+where e.scode=s.scode and p.pcode=s.advisor;
 
+select * from enr_stu_pro;
 
+/*각 학생들(학번,학생명)에 대해 수강신청 과목들의 평균 점수를 구하시오.*/
+select scode,sname,avg(grade),pname,dept
+from enr_stu_pro
+where dept='전산' or dept='전자'
+group by scode,sname,pname,dept
+having avg(grade)>=75
+order by avg(grade) desc;
 
+/*수강신청 한 과목들을 학생별로 그룹핑하여 평균 점수를 구한 다음, 학생번호, 평균 점수를 성적이 높은 순으로 정렬하여 출력하시오.*/
+select scode, avg(grade) from enrollments group by scode order by avg(grade) desc;
 
+/*수강신청 과목들의 평균 점수가 85점 이상인 학생들의 학생번호, 평균 점수를 구하시오.*/
+select scode, avg(grade) from enrollments group by scode having avg(grade) >= 85;
 
+/*강좌별 평균점수가 80점 이상인 강좌들의 강좌번호와 평균점수를 출력하시오.*/
+select lcode, avg(grade) from enrollments group by lcode having avg(grade) >= 80;
 
+/*학생수가 3명 이상인 학과 구한 다음, 학과명, 학생수를 출력하시오.*/
+select dept, count(scode) from students group by dept having count(scode) >= 3;
 
+/*수강신청 평균점수가 85점 이상인 학생들의 학생번호, 학생이름, 평균 점수를 평균점수가 높은 순으로 출력하시오.*/
+select s.scode, sname, avg(grade) from students s, enrollments e where s.scode=e.scode 
+group by s.scode, sname having avg(grade) >= 85 order by avg(grade) desc;
 
+/*강좌별 평균점수가 80점 이상인 강좌들의 강좌번호, 강좌명, 평균점수를 평균점수가 높은 순으로 출력하시오.*/
+select s.scode, sname, avg(grade) from students s, enrollments e where s.scode=e.scode 
+group by s.scode, sname having avg(grade) >= 85
+order by avg(grade) desc;
 
+/*4.조회*/
+/*'알고리즘'을 강의하는 교수의 교수번호, 교수이름, 소속학과를 검색하시오.*/
+select*from professors where pcode=
+(select instructor from courses where lname='알고리즘');
 
+/*강의실 '510'호에서 강의하는 교수의 교수번호, 교수이름, 소속학과를 검색하시오.*/
+select * from professors where pcode in
+(select instructor from courses where room='510');
+/*'김창덕' 학생이 소속된 학과에 재직하는 교수들의 이름, 직급, 임용일자를 검색하시오.*/
+select * from professors where dept =
+(select dept from students where sname='김창덕');
+/*수강신청 과목의 점수가 90점 이상인 학생들의 이름, 학생번호, 소속학과, 학년을 검색하시오.*/
+select * from students where scode in
+(select scode from enrollments group by scode having avg(grade)>=80);
 
+/*1.갱신*/
+/*'98414022', '노진순', '75-05-10', '전산' 값을 학생테이블에 삽입하시오.*/
+insert into students(scode, sname, birthday, dept)
+values('98414022', '노진순', '75-05-10', '전산');
+select * from students;
 
+/*'1998년 1월 1일' 이전에 발생한 모든 수강신청 데이터를 oldEnrollments테이블로 복사하시오.*/
+insert into oldEnrollments(scode,lcode,edate,grade)
+select scode,lcode,edate,grade from enrollments where edate <= '1998/03/03';
+select * from oldEnrollments;
 
+/*4학년 학생들의 모든 학생 데이터를 oldstudents 테이블로 복사하시오.*/
+insert into oldstudents
+select * from students where year=4;
+select * from oldstudents;
 
+/*2.갱신*/
+/*'이재광' 교수가 지도하는 학생들의 지도교수를 교수번호 '221'로 변경하시오.*/
+update students set advisor='221'
+where advisor=(select pcode from professors where pname='이재광');
+select * from students;
 
-
-
-
-
+/*'파일처리론' 과목을 수강신청 한 학생들의 점수를 5점씩 증가 시키시오. */
+update enrollments set grade=grade+5
+where lcode=(select lcode from courses where lname='파일처리론');
+select * from enrollments;
+/*'전자'과 학생들이 수강신청 한 수강신청 데이터의 점수를 0점 처리 하시오.*/
+/*'서연우' 학생의 지도교수가 강의하는 강좌의 강의실을 '304'호로 변경하시오.*/
+/*'논리회로'를 강의하는 교수의 급여를 100000원 인상하시오.*/
+/*수강신청인원수가 80명 이상인 강좌를 강의하는 교수들의 급여를 100000원 인상하시오.*/
+/*서연우' 학생의 모든 과목 점수를 5점씩 감소 시키시오.*/
+/*'전산'과 3학년 학생들이 수강신청 한 과목들의 성적을 5점씩 증가 시키시오.*/
+/*'전산'과 '부교수'가 강의하는 강의시간수를 1씩 증가 시키시오.*/
+/*수강신청 한 과목이3과목 이상인 학생들의 학년을 1씩 증가 시키시오.*/
+/*수강신청 평균점수가 80점 미만인 학생들의 학년을 1씩 감소시키시오.*/
+/*'파일처리론'을 수강신청 한 학생들의 학과를 '건축'으로 수정하시오.*/
+/*강좌별 평균점수가 80점 이상인 과목들의 강의실을 '509'호로 변경하시오.*/
+/*'오문환' 교수가 강의하는 강좌를 신청한 수강신청 데이터를 삭제하시오.*/
+/*'자료구조'를 수강신청 한 학생의 학년을 1씩 증가 시키시오.*/
